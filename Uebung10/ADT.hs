bbbc59cbe05a4466a651b54a5f6850f9324c25bd
KasumigaokaUtaha <blacklotussaki@gmail.com>
2018-01-19 11:04:28 +0100

    improve function datenIntervalle

--- Uebung10/ADT.hs
+++ Uebung10/ADT.hs
@@ -51,38 +51,46 @@ t3 :: [MultTree Int]
 t3 = [Node 3 15 [Leaf 3, Leaf 11, Leaf 12], Node 19 42 [Leaf 42, Leaf 23]]
 
 t4 :: MultTree Int
-t4 = Node 3 15 [Leaf 3, Leaf 11, Leaf 12]
+t4 = Node 3 15 [Leaf 3, Leaf 11, Leaf 12, Node 1 2 []]
+
+t5 :: MultTree Int
+t5 = Node 3 15 []
 
 verzweigungsgrad :: MultTree a -> Int
-verzweigungsgrad (Leaf _)      = 0
-verzweigungsgrad (Node _ _ xs) = verzweigungsgradHilf xs
+verzweigungsgrad (Leaf _)               = 0
+verzweigungsgrad (Node _ _ xs)          = verzweigungsgradHilf xs
 
 verzweigungsgradHilf :: [MultTree a] -> Int
-verzweigungsgradHilf [] = 0
-verzweigungsgradHilf xs = maximum(length(xs) : map verzweigungsgrad xs)
+verzweigungsgradHilf []             = 0
+verzweigungsgradHilf xs             = maximum(length(xs) : map verzweigungsgrad xs)
 
 datenListe :: MultTree a -> [a]
-datenListe (Leaf value)        = [value]
-datenListe (Node _ _ (x : xs)) = (datenListe x) ++ (datenListeHilf xs)
+datenListe (Leaf value)             = [value]
+datenListe (Node _ _ (x : xs))      = (datenListe x) ++ (datenListeHilf xs)
 
 datenListeHilf :: [MultTree a] -> [a]
-datenListeHilf []       = []
-datenListeHilf (x : xs) = (datenListe x) ++ (datenListeHilf xs)
+datenListeHilf []                   = []
+datenListeHilf (x : xs)             = (datenListe x) ++ (datenListeHilf xs)
 
 datenIntervalle :: MultTree Int -> MultTree Int
-datenIntervalle (Leaf x)      = Leaf x
-datenIntervalle (Node x y trees) = multTreeMaxMin(Node x y (datenIntervalleHilf trees))
+datenIntervalle (Leaf x)                = Leaf x
+datenIntervalle (Node x y [])           = Node minBound maxBound []
+datenIntervalle (Node x y trees)        = Node a b tree
+                                                where   tree = datenIntervalleHilf(trees)
+                                                        a = minimum(getValue tree)
+                                                        b = maximum(getValue tree)
+
+--multTreeMaxMin(Node x y (datenIntervalleHilf trees))
 
 datenIntervalleHilf :: [MultTree Int] ->[MultTree Int]
-datenIntervalleHilf ((Leaf value) : []) = [Leaf value]
-datenIntervalleHilf ((Node x y trees): []) = [multTreeMaxMin(Node x y (datenIntervalleHilf trees))]
-datenIntervalleHilf ((Leaf value) : multTrees) = (Leaf value) : (datenIntervalleHilf multTrees)
-datenIntervalleHilf ((Node x y trees): multTrees) = multTreeMaxMin(Node x y (datenIntervalleHilf trees)) : (datenIntervalleHilf multTrees)
+datenIntervalleHilf []                              = []
+datenIntervalleHilf ((Leaf value) : [])             = [Leaf value]
+datenIntervalleHilf ((Node x y trees): [])          | null trees   =   [datenIntervalle(Node x y trees)]
+                                                    | otherwise     =   [datenIntervalle(Node x y (datenIntervalleHilf trees))]
+datenIntervalleHilf ((Leaf value) : multTrees)      = (Leaf value) : (datenIntervalleHilf multTrees)
+datenIntervalleHilf ((Node x y trees): multTrees)   | null trees   = datenIntervalle(Node x y trees) : datenIntervalleHilf(multTrees)
+                                                    | otherwise     = datenIntervalle(Node x y (datenIntervalleHilf trees)) : (datenIntervalleHilf multTrees)
 
-multTreeMaxMin :: MultTree Int -> MultTree Int
-multTreeMaxMin (Node x y trees)     = Node a b trees
-                                            where   a = minimum(getValue trees)
-                                                    b = maximum(getValue trees)
 
 getValue :: [MultTree Int] -> [Int]
 getValue ((Leaf value) : [])      = [value]
